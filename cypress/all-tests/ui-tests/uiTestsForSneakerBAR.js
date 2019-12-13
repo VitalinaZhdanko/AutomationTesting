4b06206a618b9438fdb2af7135a0b9d5c7d54d30
Vitalina <vita.zhdanko@mail.ru>
2019-12-13 23:19:48 +0300

    realise_mapVSobject

--- cypress/all-tests/ui-tests/uiTestsForSneakerBAR.js
+++ cypress/all-tests/ui-tests/uiTestsForSneakerBAR.js
@@ -1,72 +1,33 @@
-import AccessoriesPageSneaker from "../../page-objects/accessoriesPageSneaker"
+import accessoriesPageSneaker from "../../page-objects/accessoriesPageSneaker"
 import Chance from 'chance'
 
-describe ('UI tests for Onliner', () => {
-    // before(() => {
-    //     cy.fixture('product').then(data=>{
-    //         cy.wrap(data).as('productData')
-    //     })
-    // })
-    // it('Positive: User is able to add one product to the card', () => {
-    //
-    //     cy.log("GIVEN User is at Product Page")
-    //     cy.visit("https://sneakers.by/katalog/obuv-belarus/kupit-krossovki-v-belarusi/nike-wmns-m2k-tekno-ao3108-403")
-    //
-    //     cy.log("WHEN User selects the size of the sneakers")
-    //     cy.get('select').select('36.5')
-    //
-    //     cy.log("AND User adds sneakers to the card")
-    //     cy.get('#button-cart').click().wait(3000)
-    //
-    //     cy.log("THEN Data product is presented in the card")
-    //     cy.get('tr').eq(2).should('contain', 'Nike WMNS M2K TEKNO AO3108-403')
-    //     cy.log("AND The size of product is egual to the selected size")
-    //     cy.get('tr').eq(2).should('contain', 'Размер (EUR) 36.5')
-    //
-    //    cy.get('.button.btn.btn-primary.btn-lg').contains('Выбрать ещё').click()
-    //     cy.reload()
+describe ('UI tests for SneakerBar', () => {
 
-   // })
+    it('Positive: User is able to add one product to the card', () => {
 
-    it('Positive: User is able to add several product to the card', () => {
+        cy.log("GIVEN User is at Product Page")
+        accessoriesPageSneaker.openProduct()
+
+        cy.log("WHEN User adds the product to the card")
+        accessoriesPageSneaker.addProduct(1)
+
+        cy.log("THEN Check results")
+        accessoriesPageSneaker.checkResults(1)
+
+        cy.log("THEN Reload page")
+        cy.reload()
+   })
+
+    it('Positive: User is able to add several products to the card from Catalog Page', () => {
 
         cy.log("GIVEN User is at Catalog Page")
-      //  cy.visit("https://sneakers.by/katalog/obuv-belarus/kupit-krossovki-v-belarusi/")
         accessoriesPageSneaker.openCatalog()
 
+        cy.log("WHEN User adds several products")
         accessoriesPageSneaker.addProduct(2)
-        // cy.get('.qw-button').contains('В корзину').eq(0).click({force: true})
-        //
-        //     cy.log("WHEN User selects the size of the sneakers")
-        //     cy.get('select#input-option704767').select("41")
-        //
-        //     cy.log("AND User adds sneakers to the card")
-        //     cy.get('#button-cart').click().wait(3000)
-        //
-        // cy.get('.button.btn.btn-primary.btn-lg').contains('Выбрать ещё').click()
-        // cy.go('back')
-        //
-        // cy.get('.qw-button').eq(1).click({force: true})
-        // cy.log("WHEN User selects the size of the sneakers")
-        // cy.get('select#input-option704766').select("45")
-        // cy.log("AND User adds sneakers to the card")
-        // cy.get('#button-cart').click().wait(3000)
-        // cy.get('.button.btn.btn-primary.btn-lg').contains('Выбрать ещё').click()
-        // cy.go('back')
-     // cy.log("WHEN User selects the size of the sneakers")
-     //    cy.get('select').select('36.5')
-     //
-     //    cy.log("AND User adds sneakers to the card")
-     //    cy.get('#button-cart').click().wait(3000)
-     //
-     //    cy.log("THEN Data product is presented in the card")
-     //    cy.get('tr').eq(2).should('contain', 'Nike WMNS M2K TEKNO AO3108-403')
-     //    cy.log("AND The size of product is egual to the selected size")
-     //    cy.get('tr').eq(2).should('contain', 'Размер (EUR) 36.5')
-     //
-     //    cy.get('.button.btn.btn-primary.btn-lg').contains('Выбрать ещё').click()
-     //    cy.reload()
 
+        cy.log("THEN Check results")
+        accessoriesPageSneaker.checkResults(2)
     })
 
 
--- cypress/page-objects/accessoriesPageSneaker.js
+++ cypress/page-objects/accessoriesPageSneaker.js
@@ -1,14 +1,22 @@
-class AccessoriesPageSneaker{
+class AccessoriesPageSneaker {
     openCatalog() {
         cy.visit(`${Cypress.env('sneakerUrl')}/katalog/obuv-belarus/kupit-krossovki-v-belarusi/`);
 
     }
 
-    addProduct(count){
-        if(count==1)
-        {}
-        else
-        {
+    openProduct() {
+        cy.visit(`${Cypress.env('sneakerUrl')}/katalog/obuv-belarus/kupit-krossovki-v-belarusi/nike-wmns-m2k-tekno-ao3108-403`);
+    }
+
+    addProduct(count) {
+        if (count == 1) {
+            cy.log("WHEN User selects the size of the sneakers")
+            cy.get('select').select('36.5')
+
+            cy.log("AND User adds sneakers to the card")
+            cy.get('#button-cart').click().wait(3000)
+        }
+        else {
             cy.log("WHEN User opens the product")
             cy.get('.qw-button').contains('В корзину').eq(0).click({force: true})
 
@@ -36,21 +44,33 @@ class AccessoriesPageSneaker{
         }
     }
 
-    get selectRAM(){
-        return cy.get('.offers-description-filter-control offers-description-filter-control_switcher');
+    checkResults(count) {
+        if (count == 1)
+        {
+            cy.log("THEN Data product is presented in the card")
+            cy.get('tr').eq(2).should('contain', 'Nike WMNS M2K TEKNO AO3108-403')
+            cy.log("AND The size of product is egual to the selected size")
+            cy.get('tr').eq(2).should('contain', 'Размер (EUR) 36.5')
+        }
+        else {
+            cy.log("THEN Data product is presented in the card")
+            cy.get('tr').eq(2).should('contain', 'Puma x BEAMS CELL Ultra (37280901)')
+            cy.log("AND The size of product is egual to the selected size")
+            cy.get('tr').eq(2).should('contain', 'Размер (EUR) 41')
+
+            cy.log("THEN Data product is presented in the card")
+            cy.get('tr').eq(4).should('contain', 'Air Jordan 1 High Zoom \'Fearless\' (BV0006-900)')
+            cy.log("AND The size of product is egual to the selected size")
+            cy.get('tr').eq(4).should('contain', 'Размер (EUR) 45')
+
+        }
     }
 
-    get searchInput(){
-        return cy.get('input[aria-label="Search Google Store"]');
-    }
 
-
-    performSearch (productToSearch){
+    performSearch(productToSearch) {
         this.searchIcon.click();
         this.searchInput.type(`${productToSearch}{enter}`);
     }
-
-
 
 
 }
